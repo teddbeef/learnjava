4f62df6b8d2834830a63d2ff10aff64c9ef31746
Tedd <91820662+teddbeef@users.noreply.github.com>
2024-11-18 23:00:42 +0700

    first commit

---
+++ src/Bai11_HomeWork.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bai11a_HomeWork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your year wanna check: ");
+        int year = sc.nextInt();
+        //check condition
+        if((year % 4 == 0) && (year % 100 !=0) || (year % 400 ==0)){
+            System.out.println(year + " is the leap year");
+        }else {
+            System.out.println(year + " isn't the leap year");
+        }
+
+
+        //Homework 2
+        System.out.println("HOMEWORK NUMBER 2");
+        System.out.println("Please enter your month wanna check: ");
+        int month = sc.nextInt();
+        //check condition
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            System.out.println("The month has 31 days");
+        }else if(month == 4 || month == 6 || month == 9 || month == 11){
+            System.out.println("The month has 30 days");
+        }else if(month == 2){
+            if((year % 4 == 0) && (year % 100 !=0) || (year % 400 ==0)){
+                System.out.println("The month has 29 days");
+            }else {
+                System.out.println("The month has 28 days");
+            }
+        }
+    }
+}
---
+++ src/Bai11_IfElse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bai11a_IfElse {
+    public static void main(String[] args) {
+        double dtb;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the grade: ");
+        dtb = sc.nextDouble();
+
+        //Check condition
+        if(dtb <= 10 && dtb>=8){
+            System.out.println("Exellent");
+        }else if(dtb<8 && dtb>=6.5){
+            System.out.println("Perfect");
+        }else if(dtb<6.5 && dtb>=5){
+            System.out.println("Ok");
+        }else{
+            System.out.println("Failed");
+        }
+    }
+}
